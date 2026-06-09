a18b54c6141c6bbdf4b323c6b5377f944b7c6130
thiago <thiagoliveira.silva@mprj.mp.br>
2026-06-09 00:12:18 -0300

    feat: implementa menu lateral de navegação (#17)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    SidebarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  menuAberto = true;
+
+  toggleSidebar(): void {
+
+    this.menuAberto = !this.menuAberto;
+
+  }
+
+  fecharSidebar(): void {
+
+    this.menuAberto = false;
+
+  }
+
 }
